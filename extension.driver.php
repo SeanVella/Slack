757a91df16351f6edfc01b772f5c1ceb695eb1d9
SeanVella <sean@maze.digital>
2017-01-23 17:21:41 +0100

    Add try-catch block

--- extension.driver.php
+++ extension.driver.php
@@ -32,43 +32,15 @@ Class extension_slack extends  Extension {
 
     public function sendSlackMessage($context) {
 
-        include_once("vendor/autoload.php");
+        try {
 
-        //Send slack message
+            include_once("vendor/autoload.php");
 
-        $hook = Symphony::Configuration()->get('web_hook', 'slack');
-        $username = Symphony::Configuration()->get('username', 'slack');
-        $channel = Symphony::Configuration()->get('channel', 'slack');
+            //Send slack message
 
-        $settings = [
-            'username' => $username,
-            'channel' => $channel
-        ];
-
-        $client = new Maknz\Slack\Client($hook, $settings);
-
-        $entryId = $context['entry']->get('id');
-
-        $author = $context['fields']['authors'];
-
-        $headline = $context['fields']['headline'];
-
-        preg_match('/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/', URL, $matches);
-
-        $link = $matches[0] . 'symphony/publish/articles/edit/'. $entryId . '/';
-
-        $client->attach([
-            "author_name" => $author,
-            "title"=> $headline,
-            "title_link"=> $link
-        ])->send('New entry created.');
-
-        //Testing purposes - send to me as well (only if config.php is not set to my hook)
-
-        if ($hook != 'https://hooks.slack.com/services/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'){
-
-            $hook = 'https://hooks.slack.com/services/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';
-            $channel = '@seanvella';
+            $hook = Symphony::Configuration()->get('web_hook', 'slack');
+            $username = Symphony::Configuration()->get('username', 'slack');
+            $channel = Symphony::Configuration()->get('channel', 'slack');
 
             $settings = [
                 'username' => $username,
@@ -77,12 +49,48 @@ Class extension_slack extends  Extension {
 
             $client = new Maknz\Slack\Client($hook, $settings);
 
+            $entryId = $context['entry']->get('id');
+
+            $author = $context['fields']['authors'];
+
+            $headline = $context['fields']['headline'];
+
+            preg_match('/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/', URL, $matches);
+
+            $link = $matches[0] . 'symphony/publish/articles/edit/'. $entryId . '/';
+
             $client->attach([
                 "author_name" => $author,
                 "title"=> $headline,
                 "title_link"=> $link
             ])->send('New entry created.');
-        }
+
+            //Testing purposes - send to me as well (only if config.php is not set to my hook)
+
+            if ($hook != 'https://hooks.slack.com/services/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'){
+
+                $hook = 'https://hooks.slack.com/services/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';
+                $channel = '@seanvella';
+
+                $settings = [
+                    'username' => $username,
+                    'channel' => $channel
+                ];
+
+                $client = new Maknz\Slack\Client($hook, $settings);
+
+                $client->attach([
+                    "author_name" => $author,
+                    "title"=> $headline,
+                    "title_link"=> $link
+                ])->send('New entry created.');
+            }
+                    
+        } catch (Exception $e) {
+
+            Symphony::Log()->pushToLog(sprintf('Slack Error: %s', $ex->getMessage()), E_NOTICE, true);
+            
+        }        
 
     }
 
